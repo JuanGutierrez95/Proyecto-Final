7752c08e4a4b1a4d47750a0c611b8e3b054db706
ale9516 <juan.gutierrez19958@gmail.com>
2023-01-30 06:40:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('button_color').addEventListener('click', function(){
+    document.body.style.backgroundColor = "#000000";
+    document.body.style.color = "#FFFFFF";
+})
+document.getElementById('button_restore').addEventListener('click', function(){
+    document.body.style.backgroundColor = "#fff2cc";
+    document.body.style.color = "#000000";
+
+})
